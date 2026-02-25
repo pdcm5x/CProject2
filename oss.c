bbd35255978caba75e3df34c9000d6f306eded21
pdcm5x@umsystem.edu <pdcm5x@umsl.edu>
2026-02-24 20:16:46 -0600

    Pseudocode

--- oss.c
+++ oss.c
@@ -2,4 +2,39 @@
 // Created by Paul Corbin on 2/24/2026.
 //
 #include "clock.h"
+#include "pcb.h"
 
+/*
+ *increment clock function (clock) {
+  *increment by 10 ms
+ *}
+ *
+ *main{
+  *int max total processes
+  *int simultaneously run
+  *int launched
+  *int currently running
+  *print oss pid
+  *initialize clock
+  *processtable[max processes]
+   *while launched < total  {
+   *increment clock
+    *if launched < total and running < simultaneous amount {
+    *pid_t pid fork process
+    *if child process {
+     *execute ./worker
+    *}
+    *update PCB table
+    *total launched ++
+    *total running++
+   *}
+   *int pid = waitpid(-1, &status, wnohang);
+   *if (pid > 0) {
+    *update process table
+    *running--
+   *}
+  *}
+  *print oss is done
+  *
+ *}
+ */
