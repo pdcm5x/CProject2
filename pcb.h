92b9372ef4f21ce23ccf0b35c30c00392a950698
pdcm5x@umsystem.edu <pdcm5x@umsl.edu>
2026-02-24 20:04:41 -0600

    Created pcb.h for the pcb struct

---
+++ pcb.h
@@ -0,0 +1,15 @@
+//
+// Created by Paul Corbin on 2/24/2026.
+//
+
+#ifndef PCB_H
+#define PCB_H
+typedef struct {
+    int occupied;
+    pid_t pid;
+    int startSeconds;
+    int startNano;
+    int endingTimeSeconds;
+    int endingTimeNano;
+} PCB;
+#endif
