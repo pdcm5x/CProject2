dcd93be508efa53ef16feb4ce3682727e2d72576
pdcm5x@umsystem.edu <pdcm5x@umsl.edu>
2026-02-24 20:32:15 -0600

    Shared memory value header

---
+++ shared.h
@@ -0,0 +1,12 @@
+//
+// Created by Paul Corbin on 2/24/2026.
+//
+
+#ifndef SHARED_H
+#define SHARED_H
+#include "clock.h"
+#define SHM_KEY 1234
+#define SHM_SIZE sizeof(Clock);
+#define SHM_PERM 0666
+
+#endif
