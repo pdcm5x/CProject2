2233b0f9fb9dab7f5d78c3b7a215eb88037ac37f
pdcm5x@umsystem.edu <pdcm5x@umsl.edu>
2026-02-24 20:24:47 -0600

    Pseudocode

--- worker.c
+++ worker.c
@@ -9,7 +9,22 @@
     *print worker pid
     *print arguements
     *
+    *Worker starting, PID:6577 PPID:6576
+    *Called with:
+    *Interval: 5 seconds, 500000 nanoseconds
     *
+    *attach to shared memory
     *
+    *calculate term time
+    *
+    *wait 1 {
+       *WORKER PID:6577 PPID:6576
+       *SysClockS: 6 SysclockNano: 45000000 TermTimeS: 11 TermTimeNano: 500100
+       *--1 seconds have passed since starting
+       *check if time to terminate
+    *}
+    *WORKER PID:6577 PPID:6576
+    *SysClockS: 11 SysclockNano: 700000 TermTimeS: 11 TermTimeNano: 500100
+    *--Terminating
  *}
  */
