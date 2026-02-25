3f5ea954172f34e7bbe75153fdc761f6a1a16e55
pdcm5x@umsystem.edu <pdcm5x@umsl.edu>
2026-02-24 19:59:47 -0600

    Created clock header

---
+++ clock.h
@@ -0,0 +1,9 @@
+#ifndef CLOCK_H
+#define CLOCK_H
+
+typedef struct {
+    unsigned int seconds;
+    unsigned int nanoseconds;
+} Clock;
+
+#endif // CLOCK_H
--- library.h
+++
@@ -1,6 +0,0 @@
-#ifndef CPROJECT2_LIBRARY_H
-#define CPROJECT2_LIBRARY_H
-
-void hello(void);
-
-#endif // CPROJECT2_LIBRARY_H
